9a388924a6974034c89904e2aca2638b38e5471d
abishekLawrence47 <abisheklawrence47@gmail>
2025-06-20 17:19:32 +0530

    parameterised Recursion

--- Recursion/sum_of_n_numbers.java
+++ Recursion/sum_of_n_numbers.java
@@ -1,7 +1,16 @@
+//sum of first n natural numbers using parameterised recursion
 public class sum_of_n_numbers {
     public static void main(String[] args) {
-        int n = 5;
-        System.out.println(n);
+           int n=3,add=0;
+           sum(n,add);
+    }
+    public static void  sum(int  n,int add){
+        if (n==0){
+            System.out.println(add);
+            return;
+            
+        }
+        sum(n-1,add+n);
     }
 }
                   
