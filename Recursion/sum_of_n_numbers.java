35161298860d009736dbdd213c56e94d4efd34ed
Abishek Lawrence <160567557+abishekLawrence47@users.noreply.github.com>
2025-07-02 17:45:09 +0530

    sum_of_n_numbers.java

--- Recursion/sum_of_n_numbers.java
+++ Recursion/sum_of_n_numbers.java
@@ -13,4 +13,20 @@ public class sum_of_n_numbers {
         sum(n-1,add+n);
     }
 }
+
+//sum of first n natural numbers using functional recursion
+public class sum_of_n_numbers{
+     public static viod main(String[] args){
+            int n=3;
+            System.out.println(sum(n));
+     }
+    public static int sum(int n){
+            if(n==0){
+                return 0;
+            }
+            return n+sum(n-1);
+    }
+}
+
+        
                   
