c3acaf805b39f60651e6ba07fd0f3a66db16247e
Abishek Lawrence <160567557+abishekLawrence47@users.noreply.github.com>
2026-01-20 19:47:43 +0530

    Hybrid inheritance example in Hybrid.java
    Implement classes for single and hierarchical inheritance.

---
+++ oops/Hybrid.java
@@ -0,0 +1,31 @@
+//single inheritance + hierarchical inheritance
+class father{
+    char gender='M';
+    void print(){
+        System.out.println("Parent class");
+    }
+}
+
+
+class daughter extends father{
+    char gender='F';
+
+}
+
+class granddaughter extends daughter{
+    
+
+}
+class son extends father{
+
+
+}
+
+public class Main{
+    public static void main(String[] args){
+        //Hierarchical inheritance
+        
+
+    }
+}
+
